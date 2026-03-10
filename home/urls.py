b53d8e8783846118062f230f982e2cc0470f0261
Abdelrahman Hassan <abdelrahmanxmohamed@gmail.com>
2026-03-10 20:51:03 +0400

    Edit the dispaly of prices , Edit the problem of direction when using Arabic add Cloudinary storage for images

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views 
-
+from products.views import all_products
 
 urlpatterns = [
-    path('home/',views.home,name='home')
+    path('home/',views.home,name='home'),
+    path('',all_products , name = 'all_products')
 ]
